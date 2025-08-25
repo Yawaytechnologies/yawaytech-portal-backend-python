8dfaeba023b76c082077dc86d66f33cec67550c2
kanaka@yawaytech.com <kanaka504@yawaytech.com>
2025-08-25 15:11:34 +0530

    changed database_url

--- app/data/db.py
+++ app/data/db.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base,Session
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
