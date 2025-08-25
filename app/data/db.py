7396d8962564c61324febf6845c1526ff07be6dd
kanaka@yawaytech.com <kanaka504@yawaytech.com>
2025-08-25 13:15:00 +0530

    added databse_url to env

--- app/data/db.py
+++ app/data/db.py
@@ -1,7 +1,9 @@
-﻿from sqlalchemy import create_engine
+﻿import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base,Session
 
-DATABASE_URL = "postgresql://myuser:yaway%40123@localhost:5432/mydb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
