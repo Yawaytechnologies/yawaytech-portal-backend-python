f8433560aa55b6642c2843b19602a916b7cb0b48
ThushyanthanJayakumar <thushyanthanbackend@gmail.com>
2026-03-10 12:55:53 +0530

    Fix black formatting in Alembic migrations

--- alembic/versions/70f32e7b51db_update_expense_category_enum_to_.py
+++ alembic/versions/70f32e7b51db_update_expense_category_enum_to_.py
@@ -52,13 +52,11 @@ def upgrade() -> None:
     new_enum.create(op.get_bind())
 
     # Alter column to use new enum
-    op.execute(
-        """
+    op.execute("""
         ALTER TABLE expenses
         ALTER COLUMN category TYPE expense_category
         USING category::text::expense_category
-    """
-    )
+    """)
 
     # Drop old enum
     op.execute("DROP TYPE expense_category_old")
@@ -69,13 +67,11 @@ def downgrade() -> None:
     old_enum.create(op.get_bind())
 
     # Revert column to old enum
-    op.execute(
-        """
+    op.execute("""
         ALTER TABLE expenses
         ALTER COLUMN category TYPE expense_category
         USING category::text::expense_category
-    """
-    )
+    """)
 
     # Drop new enum
     op.execute("DROP TYPE expense_category")
