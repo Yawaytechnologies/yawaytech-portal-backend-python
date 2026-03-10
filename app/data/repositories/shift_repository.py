7eb1e8ebccf6fddc585067ecd98c962379750d5b
ThushyanthanJayakumar <thushyanthanbackend@gmail.com>
2026-03-10 10:54:14 +0530

    Shift_department

--- app/data/repositories/shift_repository.py
+++ app/data/repositories/shift_repository.py
@@ -28,3 +28,10 @@ def get_current_shift(db: Session, employee_id: str, date) -> Shift | None:
         )
         .first()
     )
+
+def get_shifts_by_department(db: Session, department_id: int):
+    return(
+        db.query(Shift)
+        .join(EmployeeShiftAssignment, Shift.id == EmployeeShiftAssignment.shift_id)
+        .all()
+    )        
--- app/routes/employee_bank_detail_router.py
+++ app/routes/employee_bank_detail_router.py
@@ -1,15 +1,15 @@
-# app/routers/employee_bank_detail_router.py
+# app/routes/employee_bank_detail_router.py
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.data.db import get_db
+from app.controllers import employee_bank_detail_controller
 from app.schemas.employee_bank_detail import (
     EmployeeBankDetailCreate,
-    EmployeeBankDetailRead,
     EmployeeBankDetailUpdate,
+    EmployeeBankDetailRead,
 )
-from app.controllers import employee_bank_detail_controller
 
-router = APIRouter(prefix="/bank-details", tags=["Employee Bank Details"])
+router = APIRouter(prefix="/bank-details", tags=["Bank Details"])
 
 
 @router.post("/", response_model=EmployeeBankDetailRead)
@@ -17,21 +17,23 @@ def create_bank_detail(data: EmployeeBankDetailCreate, db: Session = Depends(get
     return employee_bank_detail_controller.create_bank_detail(db, data)
 
 
-@router.get("/{employee_id}", response_model=EmployeeBankDetailRead)
-def get_bank_detail(employee_id: str, db: Session = Depends(get_db)):
-    return employee_bank_detail_controller.get_bank_detail(db, employee_id)
-
-
 @router.get("/", response_model=list[EmployeeBankDetailRead])
-def list_bank_details(db: Session = Depends(get_db)):
-    return employee_bank_detail_controller.list_bank_details(db)
+def get_all_bank_details(db: Session = Depends(get_db)):
+    return employee_bank_detail_controller.get_all_bank_details(db)
+
+
+@router.get("/{employee_id}", response_model=EmployeeBankDetailRead)
+def get_bank_detail_by_employee_id(employee_id: str, db: Session = Depends(get_db)):
+    return employee_bank_detail_controller.get_bank_detail_by_employee_id(db, employee_id)
 
 
 @router.put("/{employee_id}", response_model=EmployeeBankDetailRead)
 def update_bank_detail(
-    employee_id: str, updates: EmployeeBankDetailUpdate, db: Session = Depends(get_db)
+    employee_id: str,
+    data: EmployeeBankDetailUpdate,
+    db: Session = Depends(get_db),
 ):
-    return employee_bank_detail_controller.update_bank_detail(db, employee_id, updates)
+    return employee_bank_detail_controller.update_bank_detail(db, employee_id, data)
 
 
 @router.delete("/{employee_id}")
