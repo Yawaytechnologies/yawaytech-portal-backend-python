df15e57de16987940104bff59d9203e06bbb491d
ThushyanthanJayakumar <thushyanthanbackend@gmail.com>
2026-03-24 15:02:49 +0530

    add_employee_id_datatype

--- app/data/models/add_employee.py
+++ app/data/models/add_employee.py
@@ -31,7 +31,7 @@ class Department(str, Enum):
 class Employee(Base):
     __tablename__ = "employees"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
 
     name: Mapped[str] = mapped_column(String(30), nullable=False)
     father_name: Mapped[str] = mapped_column(String(30), nullable=False)
